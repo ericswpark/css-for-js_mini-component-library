b3a6ea2ca24d148fe7fab3b670f91efb12cb9593
Eric Park <me@ericswpark.com>
2024-11-24 14:53:31 -0500

    Attempt at IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,34 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const fontSize = size === 'large' ? 1.5 : 1;
+  const iconSize = size === 'large' ? 24 : 16;
+
+  return <Wrapper style={{ '--width': width + 'px' }}>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <NativeInput placeholder={placeholder} style={{ '--icon-left': iconSize + 'px', '--font-size': fontSize + 'rem'}} />
+    <Icon id={icon} size={iconSize}/>
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: var(--width);
+  display: inline-block;
+  isolation: isolate;
+`
+
+const NativeInput = styled.input`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding-left: var(--icon-left);
+  z-index: -1;
+  border: none;
+  border-bottom: 2px solid ${COLORS.black};
+  font-size: var(--font-size);
+`;
 
 export default IconInput;
